664448639868d4b909f85c4cc08097143019d050
toqeerzia123 <toqeerzia123@gmail.com>
2022-11-28 19:56:04 +0500

    Package

---
+++ aspnet-core/src/TGC.CMS.Core/Packages/Package.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGC.CMS.Packages
+{
+    public class Package : FullAuditedEntity<int>
+    {
+        public int? NoOfTokens { get; set; }
+        public double Discount { get; set; }
+        public double UnitPrice { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
