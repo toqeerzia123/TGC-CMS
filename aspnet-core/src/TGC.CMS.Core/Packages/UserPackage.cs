d01355f8bcfdd60743959cad1737f2b8572cdc1d
toqeerzia123 <toqeerzia123@gmail.com>
2022-12-09 00:24:52 +0500

    UserPackages

---
+++ aspnet-core/src/TGC.CMS.Core/Packages/UserPackage.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.CMS.Authorization.Users;
+using TGC.CMS.CMS_Post;
+
+namespace TGC.CMS.Packages
+{
+    public class UserPackage : FullAuditedEntity<int>
+    {
+        [ForeignKey("Package")]
+        public int PackageId { get; set; }
+        public Package Package { get; set; }
+
+        [ForeignKey("User")]
+        public long UserId { get; set; }
+        public User User { get; set; }
+    }
+}
